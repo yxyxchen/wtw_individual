56dd06068495b1b4e201dc2d82cfc56d88a4997a
yxyxchen <41679242+yxyxchen@users.noreply.github.com>
2018-09-27 14:01:44 -0400

    update optimGoa;

--- optimGoal.R
+++ optimGoal.R
@@ -1,5 +1,5 @@
-# use vaWait(s1) to replace vaQuit
-optimGoal = function(para, MSPara, otherPara, cond){
+# add number of repeation
+optimGoal = function(para, MSPara, otherPara, cond, nRep = 1){
   # 
   phi = para[1]
   tau = para[2]
@@ -28,86 +28,93 @@ optimGoal = function(para, MSPara, otherPara, cond){
   # actionList
   actionList = c('wait', 'quit')
   
-  # initialize action value, eligibility trace and stat
-  ws = rep(0, nMS) # weight vector for "wait"
-  es = rep(0, nMS); # es vector for "wait"
-  onsetXs = dnorm(traceValues[1], MSMus, sigma) * sigma * traceValues[1]
-  xs = onsetXs
-  
-  # initialize time and reward seq
+  ########### simulation repeatedly ############
+  # initialize outputs
+  outputBuffer = rep(NA, nRep)
+  s = 0
+  while(s < nRep){
+    s = s + 1
+    # initialize action value, eligibility trace and stat
+    ws = rep(0, nMS) # weight vector for "wait"
+    es = rep(0, nMS); # es vector for "wait"
+    onsetXs = dnorm(traceValues[1], MSMus, sigma) * sigma * traceValues[1]
+    xs = onsetXs
+    
+    # initialize time and reward seq
     totalSecs = 0
     seq = c()
     rewardDelays = rep(0, 400)
     tIdx = 0
     
-  # initialize outputs 
-  trialEarnings = rep(0, 400)
-  timeWaited = rep(0, 400)
-
-  # loop until time runs out
-  while (totalSecs < blockSecs) {
-    tIdx = tIdx + 1
-    # sample rewardDelay
-    rewardOutputs = drawSample(cond, seq)
-    rewardDelay = rewardOutputs[['delay']]
-    seq = rewardOutputs[['seq']]
-    for(t in 1 : nTimeStep){
-      # calculte action value 
-      vaQuit = onsetXs %*% ws * gamma # didn't consider iTi, to stop getting things to complex
-      vaWait = xs %*% ws;
-      if(t > holdOnSteps){
-        # determine action
-        waitRate = exp(vaWait * tau) / sum(exp(vaWait) * tau + exp(vaQuit) * tau)
-        # when gamma is large, sometimes vaWait will be very large, sothat waitRate is NA
-        if(is.na(waitRate)){
-          waitRate = 1
-        }
-        action = ifelse(runif(1) < waitRate, 'wait', 'quit')
-      }else{
-        action = 'wait'  
-      }
-      
-      # next reward 
-      # determine whether reward occurs in the step t
-      rewardOccur = rewardDelay < timeTicks[t + 1] &&
-        rewardDelay >= timeTicks[t] 
-      # if rewarded and wait, 5; otherwise, 0
-      nextReward = ifelse(action == 'wait' && rewardOccur, 5, 0) 
-  
-      # dertime next state
-      # go to the terminate state if at the final step or quit or reward arrives
-      if(action == 'wait' && !rewardOccur && t < nTimeStep){
-        nextXs = dnorm(traceValues[t + 1], MSMus, sigma) * sigma * traceValues[t + 1]
-      }else{
-        nextXs  = onsetXs
-      }
-   
-      # update eligilibity trace
-      es =  gamma * lambda * es + xs * (action == "wait")
-      
-      # update action value of quit and wait
-      delta = nextReward + gamma * max(nextXs %*% ws, vaQuit) - ifelse(action == 'wait', vaWait, vaQuit)
-      ws = ws + phi * delta * es
-      
-      # update xs
-      xs = nextXs
-      
-      # break if arrives at onsetXs
-      # return output
-      if(action == 'quit' || rewardOccur || t == nTimeStep){
-        trialEarnings[tIdx] = ifelse(nextReward == 5, 5, 0);
-        timeWaited[tIdx] = ifelse(rewardOccur,  NA, timeTicks[t + 1])
-        rewardDelays[tIdx] = rewardDelay
-        break
-      }
-    }  # loop across timesteps
-    # update totalTimes
-    totalSecs = totalSecs + itiSecs + ifelse(rewardOccur, rewardDelay, timeWaited[tIdx])
-  } # loop across trials
-    # since time runs out at the last trial 
-    #return(-sum(trialEarnings[2 : length(trialEarnings)]))
+    # initialize outputs 
+    trialEarnings = rep(0, 400)
+    timeWaited = rep(0, 400)
     
-    # 
-    outputs = list("ws" = ws, "totalEarnings" = sum(trialEarnings[2 : length(trialEarnings)]))
-    return(outputs)
+    # loop until time runs out
+    while (totalSecs < blockSecs) {
+      tIdx = tIdx + 1
+      # sample rewardDelay
+      rewardOutputs = drawSample(cond, seq)
+      rewardDelay = rewardOutputs[['delay']]
+      seq = rewardOutputs[['seq']]
+      for(t in 1 : nTimeStep){
+        # calculte action value 
+        vaQuit = onsetXs %*% ws * gamma # didn't consider iTi, to stop getting things to complex
+        vaWait = xs %*% ws;
+        if(t > holdOnSteps){
+          # determine action
+          waitRate = exp(vaWait * tau) / sum(exp(vaWait) * tau + exp(vaQuit) * tau)
+          # when gamma is large, sometimes vaWait will be very large, sothat waitRate is NA
+          if(is.na(waitRate)){
+            waitRate = 1
+          }
+          action = ifelse(runif(1) < waitRate, 'wait', 'quit')
+        }else{
+          action = 'wait'  
+        }
+        
+        # next reward 
+        # determine whether reward occurs in the step t
+        rewardOccur = rewardDelay < timeTicks[t + 1] &&
+          rewardDelay >= timeTicks[t] 
+        # if rewarded and wait, 5; otherwise, 0
+        nextReward = ifelse(action == 'wait' && rewardOccur, 5, 0) 
+        
+        # dertime next state
+        # go to the terminate state if at the final step or quit or reward arrives
+        if(action == 'wait' && !rewardOccur && t < nTimeStep){
+          nextXs = dnorm(traceValues[t + 1], MSMus, sigma) * sigma * traceValues[t + 1]
+        }else{
+          nextXs  = onsetXs
+        }
+        
+        # update eligilibity trace
+        es =  gamma * lambda * es + xs * (action == "wait")
+        
+        # update action value of quit and wait
+        delta = nextReward + gamma * max(nextXs %*% ws, vaQuit) - ifelse(action == 'wait', vaWait, vaQuit)
+        ws = ws + phi * delta * es
+        
+        # update xs
+        xs = nextXs
+        
+        # break if arrives at onsetXs
+        # return output
+        if(action == 'quit' || rewardOccur || t == nTimeStep){
+          trialEarnings[tIdx] = ifelse(nextReward == 5, 5, 0);
+          timeWaited[tIdx] = ifelse(rewardOccur,  NA, timeTicks[t + 1])
+          rewardDelays[tIdx] = rewardDelay
+          break
+        }
+      }  # loop across timesteps
+      # update totalTimes
+      totalSecs = totalSecs + itiSecs + ifelse(rewardOccur, rewardDelay, timeWaited[tIdx])
+    } # loop across trials
+    # since time runs out at the last trial 
+    outputBuffer[s] = sum(trialEarnings[2 : length(trialEarnings)]);
+  }# end of simulations
+  return(mean(outputBuffer))
+  # outputs = list("ws" = ws, "totalEarnings" = sum(trialEarnings[2 : length(trialEarnings)]))
+  # return(outputs)
+  
 }# end of the function
